e46dfd81415ac618ac0bdfde7cbdd39e36fe70d2
omgodzilla10 <trevorhoef@gmail.com>
2016-01-16 12:31:51 -0800

    Added room table, edited spool file name

--- Asn_01_Script.sql
+++ Asn_01_Script.sql
@@ -1,6 +1,6 @@
 SET ECHO ON
 
-SPOOL C:\Users\Trevor\Documents\GitHub\sqlAsn_01\Asn_01_Spool.txt
+SPOOL C:\Users\Trevor\Documents\GitHub\sqlAsn_01\Asn1_HoefslootTrevor_StannusOliver_DarrenChan.txt
 
 ALTER SESSION SET NLS_DATE_FORMAT='YYYY-MM-DD';
 
@@ -10,13 +10,17 @@ DROP TABLE Booking;
 DROP TABLE Guest;
 
 CREATE TABLE Hotel
-(hotelNo		CHAR(8)			NOT NULL
+(hotelNo		DECIMAL(8)		NOT NULL
 ,hotelName		VARCHAR2(30)	NOT NULL
 ,city			VARCHAR2(20)	NOT NULL
 ,CONSTRAINT PKHotel PRIMARY KEY (hotelNo)
 );
 
 CREATE TABLE Room
-(
-
+(roomNo			DECIMAL(4)		NOT NULL
+,hotelNo		DECIMAL(8)		NOT NULL
+,'type'			VARCHAR2(20)	NOT NULL
+,price			DECIMAL(10, 2)	NOT NULL
+,CONSTRAINT PKRoom PRIMARY KEY (roomNo)
+,CONSTRAINT FKHotelNo FOREIGN KEY (hotelNo) REFERENCES Hotel (roomNo)
 );
